6bc9b09d96e8c6cccc0742f5aa4a77032c6a49ad
Paul Jolly <paul@myitcv.io>
2017-07-27 14:25:48 +0100

    Add support for _Imm_ types; add multi-flag to ignore package paths

--- main.go
+++ main.go
@@ -1,21 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"path/filepath"
+	"sort"
+	"strings"
 
 	"github.com/kisielk/gotool"
 )
 
+const (
+	ImmPrefix = "_Imm_"
+)
+
+type ignorePaths []string
+
+func (i *ignorePaths) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+func (i *ignorePaths) String() string {
+	return fmt.Sprint(*i)
+}
+
+var fIgnorePaths ignorePaths
+
+func init() {
+	flag.Var(&fIgnorePaths, "I", "Package path to ignore (can appear multiple times)")
+}
+
+type match struct {
+	path string
+	name string
+}
+
 func main() {
+	flag.Parse()
 	pkgs := gotool.ImportPaths([]string{"./..."})
 
 	fset := token.NewFileSet()
 
+	matches := make(map[match]string)
+
+Parse:
 	for _, dir := range pkgs {
+		for _, p := range fIgnorePaths {
+			if dir == p {
+				continue Parse
+			}
+		}
 		pkgs, err := parser.ParseDir(fset, dir, nil, 0)
 		if err != nil {
 			panic(err)
@@ -37,11 +75,43 @@ func main() {
 
 							path := filepath.Join(base, pn)
 
-							fmt.Printf("%v: ./%v.%v\n", fset.Position(s.Pos()), path, s.Name.Name)
+							key := match{
+								path: path,
+								name: s.Name.Name,
+							}
+
+							matches[key] = fset.Position(s.Pos()).String()
 						}
 					}
 				}
 			}
 		}
+	}
+
+	var out []string
+
+	for k, v := range matches {
+		if strings.HasPrefix(k.name, ImmPrefix) {
+			continue
+		}
+
+		imm, ok := matches[match{
+			path: k.path,
+			name: ImmPrefix + k.name,
+		}]
+
+		pos := v
+
+		if ok {
+			pos = imm
+		}
+
+		out = append(out, fmt.Sprintf("%v: ./%v.%v", pos, k.path, k.name))
+	}
+
+	sort.Strings(out)
+
+	for _, v := range out {
+		fmt.Println(v)
 	}
 }
